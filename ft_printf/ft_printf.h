45b82390bf737a88f9711a430d7920375e2187fd
Cafar <rafacarneros@gmail.com>
2022-10-12 16:32:33 +0200

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarnero <rcarnero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 18:49:47 by rcarnero          #+#    #+#             */
+/*   Updated: 2022/10/12 11:27:39 by rcarnero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_printf(char const *str, ...);
+int		ft_format_check(char const str, va_list things);
+int		ft_format_char(int c);
+int		ft_format_str(char *s);
+int		ft_format_dec(long num);
+int		ft_format_ptr(unsigned long long ptr);
+int		ft_hexabase(unsigned long long num, int flag);
+int		ft_format_dec(long num);
+int		ft_count_dec(long num);
+int		ft_format_undec(long num);
+int		ft_strlen(char *ptr);
+void	ft_putchar(int c);
+void	ft_putstr(char *s);
+#endif
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -1,34 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcarnero <rcarnero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/28 18:37:06 by rcarnero          #+#    #+#             */
-/*   Updated: 2022/10/02 16:00:07 by rcarnero         ###   ########.fr       */
+/*   Created: 2022/10/04 17:24:58 by rcarnero          #+#    #+#             */
+/*   Updated: 2022/10/12 10:51:37 by rcarnero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-int	ft_atoi(const char *str)
+int	ft_strlen(char *ptr)
 {
 	int	i;
-	int	is_neg;
-	int	res;
 
+	if (!ptr)
+		return (6);
 	i = 0;
-	is_neg = 1;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	while (ptr[i] != '\0')
 		i++;
-	if (str[i] == '-')
-		is_neg = -1;
-	if (is_neg == -1 || str[i] == '+')
-		i++;
-	res = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-		res = (res * 10) + (str[i++] - '0');
-	return (res * is_neg);
+	return (i);
+}
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{	
+	if (!s)
+	{
+		ft_putstr("(null)");
+		return ;
+	}
+	while (*s != '\0')
+	{
+		write(1, s, 1);
+		s++;
+	}
 }
